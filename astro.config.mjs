6e31a75e8495dba9d74102c67d7474006a6bfacb
Diego Steiner <diego.steiner@u041.ch>
2023-07-02 12:17:31 +0000

    refactor: use tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.heimv.ch',
-experimental: {
-   assets: true
-  }
+  site: 'https://www.heimv.ch',
+  experimental: {
+    assets: true
+  },
+  integrations: [tailwind()]
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			primary: '#7575d1',
+			white: '#fafafa',
+			dark: '#59599e',
+			black: '#2e2e52'
+    },
+	},
+	plugins: [],
+}
