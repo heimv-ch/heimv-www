cb9c819f64fece269fce9ce212452780e24fb89a
Diego Steiner <diego.steiner@u041.ch>
2023-09-25 12:04:28 +0000

    chore: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.heimv.ch',
-  experimental: {
-    assets: true
-  },
   integrations: [tailwind()]
 });
