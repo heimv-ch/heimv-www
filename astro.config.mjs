8e00f133e53602a4d1dc7c7f7eabb9810616395d
Diego Steiner <diego.steiner@u041.ch>
2025-07-28 11:04:33 +0000

    fix: linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
-import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
